6b7f8f0828e60015deb7867003f5a87ab152de06
Bruno Feijó <bruno.feijo@live.com>
2021-06-14 17:56:07 -0300

    Add Cors to Access-Control-Allow

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 import './config/connection';
 
 class App{
     constructor(){
         this.app = express();
-        this.app.use(express.json());
+        this.middlewares();
         this.routes();
     }
+
+    middlewares(){
+        this.app.use(express.json());
+
+        this.app.use((req, res, next) => {
+            res.header("Access-Control-Allow-Origin", "*");
+            res.header("Access-Control-Allow-Methods", 'GET, PUT, POST, DELETE'); 
+            res.header("Access-Control-Allow-Headers", 'X-PINGOTHER, Content-Type');
+            this.app.use(cors());
+            next();
+        })       
+
+    }
+    
     routes(){
         this.app.use(routes);
     }
